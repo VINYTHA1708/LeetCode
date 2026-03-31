c6b7d80a271152fa989abebc1acabbbbebd5accb
VINYTHA1708 <vinythavenkatachalam@gmail.com>
2026-03-31 11:50:46 +0530

    Implement MyHashMap class with basic operations

---
+++ 706_designHashMap.py
@@ -0,0 +1,20 @@
+class MyHashMap(object):
+    DEFAULT_SIZE = 100001
+    def __init__(self):
+        self.map = [0] * self.DEFAULT_SIZE
+        self.present = [False] * self.DEFAULT_SIZE
+
+    def put(self, key, value):
+        self.map[key] = value
+        self.present[key] = True
+        
+
+    def get(self, key):
+        if self.present[key]:
+            return self.map[key]
+        else:
+            return -1
+
+    def remove(self, key):
+        self.present[key] = False
+        
